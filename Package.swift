dba07730354caaa1febaedf7255a3e42de819409
Asatrian David <davabro98@gmail.com>
2025-12-04 20:15:01 +0200

    Add Swift Package.swift and basic test for CI

---
+++ Package.swift
@@ -0,0 +1,21 @@
+import PackageDescription
+
+let package = Package(
+    name: "MyMobileApp_Student2",
+    products: [
+        .executable(
+            name: "MyMobileApp_Student2",
+            targets: ["MyMobileApp_Student2"]
+        ),
+    ],
+    targets: [
+        .executableTarget(
+            name: "MyMobileApp_Student2",
+            path: "."
+        ),
+        .testTarget(
+            name: "MyMobileApp_Student2Tests",
+            path: "Tests"
+        ),
+    ]
+)
