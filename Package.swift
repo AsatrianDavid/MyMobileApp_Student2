39dad3f5492b95af2a1d225b45c6826ee43a688f
Asatrian David <davabro98@gmail.com>
2025-12-04 21:09:35 +0200

    fix package

--- Package.swift
+++ Package.swift
@@ -3,22 +3,38 @@ import PackageDescription
 
 let package = Package(
     name: "MyMobileApp_Student2",
-    products: [
-        .executable(
-            name: "MyMobileApp_Student2",
-            targets: ["MyMobileApp_Student2"]
-        ),
+    platforms: [
+        .iOS(.v15),    
+        .macOS(.v12)   
     ],
-    targets: [
-        .executableTarget(
+    products: [
+        .library(
             name: "MyMobileApp_Student2",
+            targets: ["MyMobileApp_Student2"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "MyMobileApp_Student2",
+            dependencies: [],
             path: ".",
-            exclude: ["Tests"]
+            exclude: [
+                "README.md",
+                "TODO.md",
+                "pr_note.txt",
+                ".github",
+                "Resources/AppIcon.png",
+                "MobileApp_Project/Info.plist"
+            ],
+            sources: [
+                "App",
+                "MobileApp_Project"
+            ]
         ),
         .testTarget(
             name: "MyMobileApp_Student2Tests",
             dependencies: ["MyMobileApp_Student2"],
-            path: "Tests/MyMobileApp_Student2Tests"
+            path: "Tests"
         ),
     ]
 )
